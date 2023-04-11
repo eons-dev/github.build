9109793ee197c5b4cb282276f703b8fe574706d8
eons <eons@eons.dev>
2023-04-11 16:34:12 -0700

    add get repos method

--- src/github.py
+++ src/github.py
@@ -18,3 +18,10 @@ class github(ebbs.Builder):
         code, user = this.RunCommand("gh api user", saveout=True)
         user = f"{user[0]}}}" #output correction, I guess.
         logging.info(f"Logged in as: {json.loads(user)['login']}")
+
+    def GetRepos(this, org):
+        # NOTE: this.RunCommand apparently doesn't capture all the repos. There seems to be a race condition in when gh actually returns and when subprocess thinks it returns.
+        result = subprocess.run(["gh", "repo" ,"list", org, "--json", "name", "--limit", "200"], stdout=subprocess.PIPE)
+        repos = json.loads(result.stdout.decode("utf-8"))
+        repos = [f"{org}/{repo['name']}" for repo in repos]
+        return repos
